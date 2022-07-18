f482b572fb30444f3e8a91a997e5d307ae999c19
anderson-mntn <88107035+anderson-mntn@users.noreply.github.com>
2022-07-18 16:50:26 -0300

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,6 +1,7 @@
 //onload mobile pizzas - nao ta mt mobile mas é pra teste do desk
 window.addEventListener('load', ()=>{
     pizzaPromoUlMob.style.display = 'none'
+    pizzaTradUlMob.style.display = 'none'
     pizzaCard.style.display = 'none'
     pizzaCard2.style.display = 'none'
 })
@@ -12,6 +13,9 @@ let pizzaPromoUl = document.querySelector('.pizza-promo-ul')
 let pizzaCard = document.querySelector('#pizza-card')
 let pizzaCard2 = document.querySelector('#pizza-card2')
 let pizzaBtnDesk = document.querySelector('#pizza-button')
+
+//Trad
+let pizzaTradUlDesk = document.querySelector('#pizza-trad-ul')
 
 //Esfiha selectors
 let esfihaButtonDesk = document.querySelector('.esfiha-button')
@@ -42,25 +46,61 @@ esfihaButtonDesk.addEventListener('click', ()=>{
 // Buttons 
 let pizzaBtnMob = document.querySelector('.pizza-button-mob')
 
-//Promo pizza
+//Promo pizza mobile
 let pizzaPromoUlMob = document.querySelector('.pizza-promo-mob')
-let pizzaPromoTitle = document.querySelector('.promo-title-mob')
+let pizzaPromoTitleMob = document.querySelector('.promo-title-mob')
+let border = document.querySelector('.border-hide3')
 
-
-
-// MOBILE ULs
 pizzaPromoUlMob.innerHTML += pizzaPromoUl.innerHTML
 
-
-
-// Functions Mobile ---------------------------------------
-
-//Promo
-pizzaPromoTitle.addEventListener("click", () =>{
+pizzaPromoTitleMob.addEventListener("click", () =>{
     if (pizzaPromoUlMob.style.display === 'none'){
         pizzaPromoUlMob.style.display = "block"
+        // border.style.width = "20%"
+        
     } else {
+        // border.style.width = "90%"
         pizzaPromoUlMob.style.display = "none"
     }
 })
+
+//Pizza Trad mobile
+let pizzaTradUlMob = document.querySelector('.pizza-trad-mob')
+let pizzaTradTitleMob = document.querySelector('.trad-title-mob')
+
+pizzaTradUlMob.innerHTML += pizzaTradUlDesk.innerHTML
+
+pizzaTradTitleMob.addEventListener("click", () =>{
+    if (pizzaTradUlMob.style.display === 'none'){
+        pizzaTradUlMob.style.display = "block"
+        // border.style.width = "20%"
+        
+    } else {
+        // border.style.width = "90%"
+        pizzaTradUlMob.style.display = "none"
+    }
+})
+
+//Especial pizza
+
+
+let pizzaEspeTitleMob = document.querySelector('.espe-title-mob')
+let pizzaEspeUlMob = document.querySelector('.pizza-espe-mob-item')
+
+let pizzaEspeUlDesk = document.querySelector('#pizza-especial-ul')
+pizzaEspeUlMob.innerHTML += pizzaEspeUlDesk.innerHTML
+
+
+
+// //Trad
+// pizzaTradTitleMob.addEventListener("click", () =>{
+//     if (pizzaTradUlMob.style.display === 'none'){
+//         pizzaTradUlMob.style.display = "block"
+//         border.style.width = "20%"
+        
+//     } else {
+//         border.style.width = "90%"
+//         pizzaTradUlMob.style.display = "none"
+//     }
+// })
 
